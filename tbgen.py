7753fb4abfd95580b668a0ab5047abddb1c2eafd
mhdahab <67865770+mhdahab@users.noreply.github.com>
2021-10-03 17:51:19 +0200

    Add files via upload

--- tbgen.py
+++ tbgen.py
@@ -1,68 +1,63 @@
 #! /usr/bin/env python 
 
-# <xfguo@xfguo.org> wrote a file that we used as a reference for this project .
 
 import re 
 import sys 
 import hdlparse.vhdl_parser as vhdl
 import hdlparse.verilog_parser as vlog
-from hdlparse.vhdl_parser import VhdlComponent
-
-
-class tbgen(object):
-
-
-	def __init__(self, infilename): #constructor 
-		self.infilename = infilename
-		self.module_name = ""
-		self.clock_name = 'clk'
-		self.reset_name = 'rst'
-		self.outputfile_name = None
-		self.open_outputfile()
-		self.inputfile=None
-		self.outfile  = sys.stdout
-		self.textinfile = ""
-		self.open_inputfile()
-		self.parser()
-		self.open_outputfile()
-		vhdl_ex = vhdl.VhdlExtractor()
-		vlog_ex = vlog.VerilogExtractor()
-		
-
-	def open_inputfile (self): #read the module file 
-		try:
-			self.inputfile = open(self.infilename, 'r')
-			self.textinfile= self.inputfile.read()
-			vlog_mods = vlog_ex.extract_objects_from_source(textinfile)
-
-		except:
-			print ("input file cannot be opened")
-			sys.exit(1)
-
-	def open_outputfile (self):#create testbench file
-		outputfile_name = "%s_tb.v" % self.module_name
-		self.outfile = open (self.outputfile_name, 'w')
-
-	def space_remover(self,text):
-		text=re.sub(r"//[^\n^\r]*", '\n', text)
-		text = re.sub(r"/\*.*\*/", '', text)
-		text = re.sub(r'    +', ' ', text)
 
 
 
 
+def sourcefile(code):
+    vlog_ex = vlog.VerilogExtractor()
+    vlog_mods = vlog_ex.extract_objects_from_source(code)
 
-	if __name__ == "__main__": #main function 
+ 
+    for i in vlog_mods :
+        outputfile_name = "%s_tb.v" % i.name
+        outputfile= open(outputfile_name,'w+')
+        outputfile.write("`timescale 1ns/1ps" + '\n')
+        outputfile.write('\n'+ "module   " + i.name + "_tb;"  + '\n')
+        outputfile.write('\n')
 
-		if len(sys.argv) == 1:
-			print ("Invailed Command Use: tbgen.py input_verilog_file_name ")
-			sys.exit(1)
-		print("Object  NOT Created")
-		testbench=tbgen(sys.argv[1])
-		print("Object Created")
-		#testbench.print_module_head()
-		#testbench.print_wires()
-		#testbench.print_dut()
-		#testbench.print_clock_gen()
-		#testbench.print_module_end()
-		#testbench.close()
+        for j in i.ports:
+            if j.mode == "input":
+                outputfile.write("reg  " + j.data_type + " "  + j.name + " ;" +'\n')
+            elif j.mode == "output":
+                outputfile.write("wire  " + j.data_type + " "  + j.name + " ;" + '\n')
+
+        outputfile.write('\n')
+        outputfile.write(i.name + "  MUV  ( ")
+        last = len(i.ports)-1
+
+        for j in i.ports:
+            if i.ports.index(j)!=last:
+                outputfile.write(j.name + "  , " )
+            else:
+                outputfile.write(j.name + "   " )
+
+        outputfile.write(" );"+'\n')
+        outputfile.write('\n')
+        outputfile.write("initial begin"+'\n')
+        outputfile.write("$dumpfile(\"" + i.name + ".vcd\");" + '\n')
+        outputfile.write("$dumpvars;"+'\n')
+        outputfile.write("end"+'\n')
+       
+
+         
+        
+        
+
+def main():
+    inputfile=open(sys.argv[1] ,"r+")
+    code=inputfile.read()
+    sourcefile(code)
+    inputfile.close()
+    print ("Testbench Created")
+
+if __name__ == "__main__": 
+    main()
+
+
+  
