08e6d748d22579ace041e6dbe6e296e7ba6eeab3
mhdahab <67865770+mhdahab@users.noreply.github.com>
2021-10-06 10:33:57 +0200

    Add files via upload

--- tbgen.py
+++ tbgen.py
@@ -9,7 +9,7 @@ import hdlparse.verilog_parser as vlog
 
 
 
-def sourcefile(code):
+def sourcefile(code , rand ):
     vlog_ex = vlog.VerilogExtractor()
     vlog_mods = vlog_ex.extract_objects_from_source(code)
 
@@ -18,17 +18,31 @@ def sourcefile(code):
         outputfile_name = "%s_tb.v" % i.name
         outputfile= open(outputfile_name,'w+')
         outputfile.write("`timescale 1ns/1ps" + '\n')
-        outputfile.write('\n'+ "module   " + i.name + "_tb;"  + '\n')
+        outputfile.write('\n'+ "module   " )
+
+        parameter = 0 ; 
+   #adding any parameters 
+    for i in vlog_mods :
+        for j in i.ports:
+            if j.name =="clk" or j.name=="clock" :
+                if parameter ==0 :
+                    outputfile.write("#(parameter clk_period = 10 ")
+                    parameter+=1
+                else :
+                    outputfile.write(" , parameter clk_period = 10 ")
+                    parameter+=1
+
+        outputfile.write(")  " + i.name + "_tb;"  + '\n')
         outputfile.write('\n')
 
         for j in i.ports:
             if j.mode == "input":
-                outputfile.write("reg  " + j.data_type + " "  + j.name + "_i ;" +'\n')
+                outputfile.write("  reg  " + j.data_type + " "  + j.name + "_i;" +'\n')
             elif j.mode == "output":
-                outputfile.write("wire  " + j.data_type + " "  + j.name + "_o ;" + '\n')
+                outputfile.write("  wire  " + j.data_type + " "  + j.name + "_o;" + '\n')
 
         outputfile.write('\n')
-        outputfile.write(i.name + "  MUV  ( ")
+        outputfile.write("  "+i.name + "  MUV  ( ")
         last = len(i.ports)-1
 
         for j in i.ports:
@@ -39,15 +53,21 @@ def sourcefile(code):
 
         outputfile.write(" );"+'\n')
         outputfile.write('\n')
-        outputfile.write("initial begin"+'\n')
-        outputfile.write("$dumpfile(\"" + i.name + ".vcd\");" + '\n')
-        outputfile.write("$dumpvars;"+'\n')
-        outputfile.write("end"+'\n')
-        outputfile.write("initial begin"+'\n')
+        outputfile.write("  initial begin"+'\n')
+        outputfile.write("      $dumpfile(\"" + i.name + ".vcd\");" + '\n')
+        outputfile.write("      $dumpvars;"+'\n')
+        outputfile.write("  end"+'\n')
+        outputfile.write("  initial begin"+'\n')
 
         for j in i.ports:
             if j.mode == "input":
-                outputfile.write(j.name+" = 0;" + '\n')
+                outputfile.write("      "+j.name+" = 0;" + '\n')
+
+        if rand =="-rand" :
+            for j in i.ports:
+                if j.mode == "input":
+                    outputfile.write("Loop for random input \n" )
+
 
        
 
@@ -55,9 +75,12 @@ def sourcefile(code):
         
 
 def main():
-    inputfile=open(sys.argv[1] ,"r+")
+    if sys.argv[1] =="-rand":
+        inputfile=open(sys.argv[2] ,"r+")
+    else :
+        inputfile=open(sys.argv[1] ,"r+")
     code=inputfile.read()
-    sourcefile(code)
+    sourcefile(code ,sys.argv[1])
     inputfile.close()
     print ("Testbench Created")
 
